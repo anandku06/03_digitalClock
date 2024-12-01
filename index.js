d4f042fd28e5c43b7380e9871c5d5ab2191cd752
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-01 23:21:19 +0530

    implement real-time clock update in index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const clock = document.getElementById('clock')
 
-let date = new Date();
-console.log(date.toLocaleTimeString())
+
+setInterval(() => {
+    let date = new Date();
+    // console.log(date.toLocaleTimeString())
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000) 
+// sets a particular function with a time interval; invokes the function again and again after each given time interval
+// takes two args; a callback function and the time interval(in ms)
